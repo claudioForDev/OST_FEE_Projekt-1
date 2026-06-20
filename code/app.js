037b95ed57ea0a5f90b2c4dcc11a058ae075c0d7
Claudio Fornito <claudio.fornito@ost.ch>
2026-06-20 11:58:38 +0200

    feat: add basic server logic

---
+++ code/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import { indexRoutes } from "./routes/index-routes.js";
+import { noteRoutes } from "./routes/note-routes.js";
+
+// import { indexRoutes } from './routes/index-routes.js';
+// import {orderRoutes} from './routes/order-routes.js';
+import { CONFIG } from "./config.js";
+
+export const app = express();
+
+app.use(express.static(CONFIG.public));
+app.use(express.json());
+
+app.use("/", indexRoutes);
+app.use("/notes", noteRoutes);
+
+app.get("/", function (req, res) {
+  res.sendFile("/index.html", { root: CONFIG.public });
+});
+
+// app.use("/", indexRoutes);
+// app.use("/orders", orderRoutes);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+  // if (err.name === 'UnauthorizedError') {
+  //     res.status(401).send('No token / Invalid token provided');
+  // } else {
+  //     next(err);
+  // }
+});
---
+++ code/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+
+// load config-file
+// dotenv.config({ path: `.env${process.env.NODE_ENV ? `-${process.env.NODE_ENV}` : ''}`});
+dotenv.config({ path: `code/.env` });
+
+console.log(process.env.HOSTNAME, process.env.PORT);
+
+// load app with current config
+const app = (await import("./app.js")).app;
+
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const port = process.env.PORT || 3001;
+
+app.listen(port, hostname, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  }
+});
